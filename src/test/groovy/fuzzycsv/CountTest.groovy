afef88e4e225b774fb6b569f96e5369a86deac8a
kayr <noryak1234@gmail.com>
2013-10-21 11:16:39 +0300

    More count tests

--- src/test/groovy/fuzzycsv/CountTest.groovy
+++ src/test/groovy/fuzzycsv/CountTest.groovy
@@ -11,15 +11,6 @@ import org.junit.Test
  */
 class CountTest {
 
-    static def csv = [
-            ['sub_county', 'ps_total_score', 'pipes_total_score', 'tap_total_score'],
-            ['Hakibale', 18.1, null, null],
-            ['Kabonero', 1, null, null],
-            ['Kisomoro', null, 1, 10],
-            ['Bunyangabu', null, null, '1'],
-            ['Noon', null, null, 0]
-    ]
-
     @Test
     void testGetValue() {
 
