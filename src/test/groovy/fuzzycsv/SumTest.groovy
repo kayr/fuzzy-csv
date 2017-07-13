179e5413761433462e2218686363b95e4dc995a3
kayr <noryak1234@gmail.com>
2017-07-13 16:39:17 +0300

    some more sum tests

--- src/test/groovy/fuzzycsv/SumTest.groovy
+++ src/test/groovy/fuzzycsv/SumTest.groovy
@@ -2,6 +2,7 @@ package fuzzycsv
 
 import org.junit.Test
 
+import static fuzzycsv.RecordFx.fn
 import static fuzzycsv.Sum.sum
 
 class SumTest {
@@ -23,9 +24,35 @@ class SumTest {
     @Test
     void testSumFunctionsColumns() {
         FxExtensions.treatNullAsZero()
-        def sumFx = new Sum(columns: [RecordFx.fn  {
+        def sumFx = new Sum(columns: [fn {
             it.'ps_total_score' + it.'pipes_total_score' + it.'tap_total_score'
-        }], columnName: 'sum', data:  Data.csv)
+        }], columnName: 'sum', data: Data.csv)
+        assert sumFx.value == 31.1
+        assert sumFx.columnName == 'sum'
+
+        //test fluent
+        sumFx = sum('ps_total_score', 'pipes_total_score', 'tap_total_score').az('sum')
+        sumFx.data = Data.csv
+
+        assert sumFx.value == 31.1
+        assert sumFx.columnName == 'sum'
+    }
+
+    @Test
+    void testSumFunctionsColumnsWithStaticApi() {
+        FxExtensions.treatNullAsZero()
+        def sumFx = new Sum(columns: [fn {
+            it.'ps_total_score' + it.'pipes_total_score' + it.'tap_total_score'
+        }], columnName: 'sum', data: Data.csv)
+
+        def tb = FuzzyCSVTable.tbl(Data.csv)
+
+        assert tb.aggregate(
+                RecordFx.fx { 'sas' }.az('asas'),
+                FuzzyStaticApi.sum(fn {
+                    it.'ps_total_score' + it.'pipes_total_score' + it.'tap_total_score'
+                }))[1][0] == 31.1
+
         assert sumFx.value == 31.1
         assert sumFx.columnName == 'sum'
 
