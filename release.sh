a9250287a39c35a80e4baaff14e90320db3eb9fb
Ronald Kayondo <noryak1234@gmail.com>
2023-07-27 18:52:40 +0000

    Release 1\.8\.5

--- release.sh
+++ release.sh
@@ -53,6 +53,7 @@ S_NEXT_VERSION=$(echo $NEXT_VERSION | sed 's/\./\\\./g')
 
 # Update the version in the README.md
 sed -i -e  "s/implementation 'io\.github\.kayr:fuzzy-csv:.*-groovy3'/implementation 'io.github.kayr:fuzzy-csv:$S_NEXT_VERSION-groovy3'/g" README.md
+sed -i -e  "s/implementation 'io\.github\.kayr:fuzzy-csv:.*-groovy4'/implementation 'io.github.kayr:fuzzy-csv:$S_NEXT_VERSION-groovy4'/g" README.md
 
 # set the version in gradle.properties
 sed -i  -e "s/VERSION_NAME=.*/VERSION_NAME=$S_NEXT_VERSION/g" gradle.properties
